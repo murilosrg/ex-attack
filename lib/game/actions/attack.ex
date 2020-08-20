7cccb0b2563f9090e54b0682139cf9f36e157edc
Sérgio Murilo da Silva Oliveira Sobrinho <muriilo@live.it>
2020-08-20 19:18:46 -0300

    added turn logic

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -12,7 +12,7 @@ defmodule ExAttack.Game.Actions.Attack do
     |> Game.fetch_player()
     |> Map.get(:life)
     |> calculate_total_life(damage)
-    |> update_player_life(opponent)
+    |> update_player_life(opponent, damage)
   end
 
   defp calculate_power(:move_avg), do: Enum.random(@move_avg_power)
@@ -21,18 +21,18 @@ defmodule ExAttack.Game.Actions.Attack do
   defp calculate_total_life(life, damage) when life - damage < 0, do: 0
   defp calculate_total_life(life, damage), do: life - damage
 
-  defp update_player_life(life, opponent) do
+  defp update_player_life(life, opponent, damage) do
     opponent
     |> Game.fetch_player()
     |> Map.put(:life, life)
-    |> update_game(opponent)
+    |> update_game(opponent, damage)
   end
 
-  def update_game(player, opponent) do
+  def update_game(player, opponent, damage) do
     Game.info()
     |> Map.put(opponent, player)
     |> Game.update()
 
-    Status.print_round_message()
+    Status.print_move_message(opponent, :attack, damage)
   end
 end
--- lib/game/status.ex
+++ lib/game/status.ex
@@ -10,4 +10,12 @@ defmodule ExAttack.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts("\n===== Invalid move: #{move} =====\n")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n===== The player attacked computer dealing #{damage} damage =====\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n===== The computer attacked player dealing #{damage} damage =====\n")
+  end
 end
